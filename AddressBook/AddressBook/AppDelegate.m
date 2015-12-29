31caaedb0c4e69871b34f63841f16108f34dccec
Andy <zhangyaqi@ihejun.com>
2015-12-29 17:38:54 +0800

    测试

--- AddressBook/AddressBook/AppDelegate.m
+++ AddressBook/AddressBook/AppDelegate.m
@@ -14,13 +14,6 @@
 {
     // Override point for customization after application launch.
     return YES;
-    
-    
-    
-    
-    
-    
-    
 }
 							
 - (void)applicationWillResignActive:(UIApplication *)application
